5b26c1574f80f74a9abcb1ccd7460a6a6cd33543
Nikita Shultais <nikita@shultais.ru>
2026-06-01 14:42:48 +0200

    Добавил SystemMessage. Установил таймауты и повторы.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 
 
 load_dotenv()
 
 
-model = ChatOpenAI(model="gpt-4.1-mini")
-response = model.invoke("Скажи 'Привет'")
+model = ChatOpenAI(model="gpt-5.4-pro", temperature=1, timeout=(10, 120), max_retries=0)
+response = model.invoke([
+    SystemMessage(content="Ты специалист по славянским языкам."),
+    HumanMessage(content="Скажи 'Привет' на трех языках"),
+])
+
+print(type(response))
+print(response.response_metadata)
+print(type(response.content))
 print(response.content)
