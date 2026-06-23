8b87ad77ccc641f476c24f9530a2d11ce803d3da
Nikita Shultais <nikita@shultais.ru>
2026-06-23 11:14:41 +0200

    Встроенные парсеры

--- Tools/currency/chains.py
+++ Tools/currency/chains.py
@@ -1,5 +1,9 @@
 from Tools.currency.prompts import currency_prompt
 from Tools.currency.models import currency_model
 
+from langchain_core.output_parsers import JsonOutputToolsParser, JsonOutputKeyToolsParser
 
-currency_chain = currency_prompt | currency_model
+tools_parser = JsonOutputKeyToolsParser(first_tool_only=True, return_id=True, strict=True, key_name="convert_currency")
+
+
+currency_chain = currency_prompt | currency_model | tools_parser
--- Tools/currency/program.py
+++ Tools/currency/program.py
@@ -5,14 +5,8 @@ from Tools.currency.chains import currency_chain
 from Tools.currency.tools import convert_currency
 
 text = input("Что конвертируем? ")
-result = currency_chain.invoke({"text": text})
-print(result)
+tool_call = currency_chain.invoke({"text": text})
 
-tool_calls = result.tool_calls
-if tool_calls:
-    for tool_call in tool_calls:
-        if tool_call["name"] == "convert_currency":
-            output = convert_currency.invoke(tool_call["args"])
-            print(output)
-else:
-    print(result.text)
+if tool_call:
+    output = convert_currency.invoke(tool_call["args"])
+    print(output)
