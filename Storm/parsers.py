c155c654f8b0ce9c65657db1460b9a6858ba4c33
Nikita Shultais <nikita@shultais.ru>
2026-06-26 11:27:57 +0200

    Изменил формат markdown

--- Storm/parsers.py
+++ Storm/parsers.py
@@ -3,13 +3,13 @@ from langchain_core.runnables import RunnableLambda
 
 
 def make_markdown_func(recipe: GeneratedRecipe):
-    md = f"# Готовим {recipe.name}\n"
-    md += f"\n## Ингредиенты\n"
+    md = f"## {recipe.name}\n"
+    md += f"\n### Ингредиенты\n"
 
     for i, ingredient in enumerate(recipe.ingredients, start=1):
         md += f"{i}. {ingredient}\n"
 
-    md += f"\n## Рецепт\n"
+    md += f"\n### Рецепт\n"
     for i, step in enumerate(recipe.steps, start=1):
         md += f"{i}. {step}\n"
 
