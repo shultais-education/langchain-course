ad1e147cf45258963a117160489b1c585fc3d5be
Nikita Shultais <nikita@shultais.ru>
2026-07-13 16:51:35 +0200

    Нарезка текстов с помощью LangChain

--- RAG/loaders/lc_war.py
+++ RAG/loaders/lc_war.py
@@ -1,20 +1,31 @@
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import MarkdownHeaderTextSplitter
 
 with open("../docs/war-and-peace-1.txt") as f:
     text = f.read()
+
+
+book_splitter = MarkdownHeaderTextSplitter(
+    headers_to_split_on=[
+        ("##", "Часть"),
+        ("###", "Глава"),
+    ]
+)
 
 chunk_size = 1000
 chunk_overlap = 150
 
 text_splitter = RecursiveCharacterTextSplitter(
-    separators=[r"\s#{2}\s.*\n", r"\s#{3}\s.*\n", "\n\n", "\n", " ", ""],
-    is_separator_regex=True,
     chunk_size=chunk_size,
-    keep_separator=False,
     chunk_overlap=chunk_overlap
 )
 
-chunks = text_splitter.split_text(text)
+book_chunks = book_splitter.split_text(text)
+chunks = text_splitter.split_documents(book_chunks)
+
 for chunk in chunks:
     print(chunk)
     print("- " * 50)
+
+print(len(book_chunks))
+print(len(chunks))
