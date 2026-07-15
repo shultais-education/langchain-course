4aafe0256e4abea80926b1226b4ad7b8d42b3d05
Nikita Shultais <nikita@shultais.ru>
2026-07-15 19:19:35 +0200

    Точный поиск в векторной базе данных

--- RAG/program.py
+++ RAG/program.py
@@ -3,6 +3,6 @@ load_dotenv()
 
 from RAG.rag.chains import rag_chain
 
-question = "поясни за шмот жены Болконского"
+question = "Кто сказал фразу: Рана не здесь, а вот где"
 answer = rag_chain.invoke({"question": question, "book": None})
 print(answer)
