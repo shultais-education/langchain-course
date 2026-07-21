826cb9260666d14a15b4109243a29ddcd3133c16
Nikita Shultais <nikita@shultais.ru>
2026-07-21 12:29:35 +0200

    Подключение DeepSeek

--- MenuAssistant/models.py
+++ MenuAssistant/models.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableLambda
+from langchain_deepseek import ChatDeepSeek
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_openai import ChatOpenAI
 from MenuAssistant.schemas import GeneratedMenu, GeneratedRecipe
@@ -10,12 +10,12 @@ load_dotenv()
 
 # Модели
 gemini_model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite", temperature=0, timeout=120, max_retries=0)
-gpt_model = ChatOpenAI(model="gpt-4.1-mini", temperature=0, timeout=120, max_retries=0)
-
+gpt_model = ChatOpenAI(model="gpt://b1gl7fhi439lm16r9jbl/yandexgpt-5-lite/latest", temperature=0, timeout=120, max_retries=0)
+deepseek_model = ChatDeepSeek(model="gpt://b1gl7fhi439lm16r9jbl/deepseek-v4-flash/latest", temperature=0, timeout=120, max_retries=0)
 
 # Структурированные модели
-choice_model = gemini_model.with_structured_output(GeneratedMenu, strict=True)
-recipe_model = gemini_model.with_structured_output(GeneratedRecipe, strict=True)
+choice_model = gpt_model.with_structured_output(GeneratedMenu, strict=True)
+recipe_model = gpt_model.with_structured_output(GeneratedRecipe, strict=True)
 
 choice_model_fallback = gpt_model.with_structured_output(GeneratedMenu, strict=True)
 recipe_model_fallback = gpt_model.with_structured_output(GeneratedRecipe, strict=True)
