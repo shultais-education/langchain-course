21da9332faae2fd5223c2eb4eecc688335fa42d1
Nikita Shultais <nikita@shultais.ru>
2026-07-22 16:54:13 +0200

    Прямой возврат от инструмента

--- RAG/tools.py
+++ RAG/tools.py
@@ -2,7 +2,7 @@ from RAG.rag.chains import rag_chain
 from langchain_core.tools import tool
 
 
-@tool("common_question", description="Общие вопросы пользователя")
+@tool("common_question", description="Общие вопросы пользователя", return_direct=True)
 def rag(question: str) -> str:
     print("rag")
     return rag_chain.invoke({"question": question, "book": None})
